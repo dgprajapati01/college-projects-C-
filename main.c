69273555504ffb2c709fd38952eb58085b32d43c
dgprajapati01 <dhyey.prajapati52@gmail.com>
2026-03-02 18:01:12 +0530

    PROJECT - 1 cALCULATOR

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1;
+    int num2;
+
+
+
+    int a = 10;
+    int b = 20;
+
+    int sum;
+    sum= a + b;
+    printf("%d", sum);
+
+    int sub;
+    sub = a - b;
+    printf("%d", sub);
+    return 0;
+}
